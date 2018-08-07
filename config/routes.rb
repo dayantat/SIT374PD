8f967e6ac9ffc7cc756c953fd96018b6299a757b
Dayan Tatlock <dane@Dayans-MacBook-Pro.local>
2018-08-07 21:03:07 +1000

    inital pages

--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,8 @@
 Rails.application.routes.draw do
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
-    root "application#hello"
+    root "pages#home"
 
+  #  get 'pages/home', to: 'pages#home'
+    get '/about', to: 'pages#about'
+    get '/sign_up', to: 'pages#sign_up'
 end
